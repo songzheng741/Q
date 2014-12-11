877ffd6e697d33ef39911da00fa9dd027a33289a
songzheng741 <329156082@qq.com>
2014-12-11 21:44:44 +0800

    删除无用资源

--- modules/core/promise.js
+++ modules/core/promise.js
@@ -1,80 +1,46 @@
 var UNFULFILLED = 'unfulfilled',
     FULFULLED = 'fulfilled',
     FAILED = 'failed';
-
+var id = 0;
 function Promise() {
     this.status = UNFULFILLED;
-    this.called = false;
-    this.callbacks = [];
-    this.result = null;
+    this.successHandler = null;
+    this.errorHandler = null;
+    this.prev = null;
+    this.next = null;
+    this.id = id++;
+    this.result;
 }
 
 Promise.prototype.resolve = function(result) {
-
-    while(this.callbacks.length) {
-        var callback = this._getHandlerByType('successHandler');
-        var _result = callback.call(this, this.result || result);
-        if (_result) {
-            if (_result instanceof Promise) {
-                this._flowToOtherPromise(_result);
-            } else {
-                this.result = _result;
-            }
-        } else {
-            this.result = result;
+    var next = this;
+    while(next) {
+        this.result = next.successHandler(result || next.prev.result);
+        if (result instanceof Promise) {
+            console.log(this.id)
+            result = next;
+            result.prev.status = FULFULLED;
+            result.prev = null;
         }
-
+        next = next.next;
     }
-
-    this.status = FULFULLED;
 }
 
 Promise.prototype.reject = function(reason) {
 
-    while(this.callbacks.length) {
-        var callback = this._getHandlerByType('errorHandler');
-        if (callback) {
-            var result = callback.call(this, reason);
-            if (result instanceof Promise) {
-                this._flowToOtherPromise(result);
-            } else {
-                this.result = result;
-            }
-        } else {
-            continue;
-        }
-    }
 
-    this.status = FAILED;
-}
-
-Promise.prototype._flowToOtherPromise = function(promise){
-    for (var i = 0; i < this.callbacks.length; i++) {
-        var fn = this.callbacks[i];
-        promise.then(fn.successHandler,fn.errorHandler);
-    }
-    this.callbacks = [];
 }
 
 Promise.prototype.then = function(successHandler, errorHandler) {
+    this.successHandler = successHandler;
+    this.errorHandler = errorHandler;
 
-    this.callbacks.push({
-        successHandler: successHandler,
-        errorHandler: errorHandler
-    });
-    if (this.status === FULFULLED) {
-        this.resolve(this.result);
-    }
-    if (this.status === FAILED) {
-        this.reject(this.result);
-    }
-    return this;
+    var next = new Promise();
+    this.next = next;
+    next.prev = this;
+
+    return next;
 }
 
-Promise.prototype._getHandlerByType = function(type) {
-
-    return this.callbacks.shift()[type];
-
-}
 
 module.exports = Promise;
