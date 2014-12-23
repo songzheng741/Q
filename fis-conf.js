8fe48e38d898539e889ffd003f91ac13ea0e7f9e
songzheng741 <13821993426@136.com>
2014-12-23 22:54:39 +0800

    新增map方法
    新增map方法

--- fis-conf.js
+++ fis-conf.js
@@ -1,3 +1,19 @@
+fis.config.set('roadmap.path',([{
+    reg: /^\/modules\/(.*)\/(.*)/i,
+    release: '/static/js/$1/$2'
+}]));
+fis.config.get('roadmap.path').push({
+    reg: /^\/modules\/pages\/(.*)/i,
+    release: '/static/pages/$1'
+});
+fis.config.get('roadmap.path').push({
+    reg: /^\/lib\/(.*)/i,
+    release: '/static/$1'
+});
+fis.config.get('roadmap.path').push({
+    reg: /^\/modules\/style\/(.*\.css)/i,
+    release: '/static/style/$1'
+});
 fis.config.set('pack', {
     'Q.js': [
         '/modules/core/**.js'
